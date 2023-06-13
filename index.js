2afbb22b62fc9bd516484044c0e5c2bf928c6a26
egorvas <egorvas@gmail.com>
2023-06-13 19:52:54 +0200

    add cron

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 const twitch = require("twitch-m3u8");
 const m3u = require('m3u');
 const express = require("express");
-const CHANNELS = process.env.CHANNELS.split(",") || ["dotatv247"];
+const cron = require('node-cron');
+const CHANNELS = process.env.CHANNELS?process.env.CHANNELS.split(","):["dotatv247"];
+const CRON = process.env.CRON || '* * * * *';
+let response = '';
+
+async function update(){
+    const writer = m3u.writer();
+    for await (const channel of CHANNELS){
+        let data;
+        try{
+            data = await twitch.getStream(channel);
+        }catch(e){
+        }
+        if (data){
+            for await (const res of data){
+                writer.write(`#EXTINF:0,${channel} - ${res.resolution?res.resolution:"audio"}`);
+                writer.write(`#EXTGRP:${res.quality}`);
+                writer.write(res.url);
+            }
+        }
+        response = writer.toString();
+    }
+}
+
 
 if (CHANNELS.length>0){
+
+    cron.schedule(CRON, async () => {
+        await update();
+    });
+
     const app = express();
     app.set('port', (process.env.PORT || 5000));
     app.get('/', async (req, res) => {
-        const writer = m3u.writer();
-        for await (const channel of CHANNELS){
-            let data;
-            try{
-                data = await twitch.getStream(channel);
-            }catch(e){
-            }
-            if (data){
-                for await (const res of data){
-                    writer.write(`#EXTINF:0,${channel} - ${res.resolution?res.resolution:"audio"}`);
-                    writer.write(`#EXTGRP:${res.quality}`);
-                    writer.write(res.url);
-                }
-            }
-        }
-        res.send(writer.toString());
+        res.send(response);
     })
 
-    app.listen(app.get('port'), () => {
-        console.log(`Starting on port ${app.get('port')}`)
+    app.listen(app.get('port'), async () => {
+        console.log(`Starting on port ${app.get('port')}`);
+        await update();
     })
 }else{
     console.log(`Invalid CHANNELS array`);
